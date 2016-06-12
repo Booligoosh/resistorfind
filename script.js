1913b173928c6adba76404faa88ea401b1aa15d0
Booligoosh <booligoosh@optusnet.com.au>
2016-06-12 11:56:35 +1000

    added band 3 changing (I think?)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var resitance = document.getElementById("resistance").value;
+var multiplier = 0;
 var tolerance = document.getElementById("tolerance").value;
+var band3colors = ["0filler", "#000000", "#6e2c00", "#ff0000", "#ff8000", "#ffff00", "#008000", "#0000ff"];
 var band4colors = ["0filler", "#6e2c00", "#ff0000", "3filler", "4filler", "#cc9933", "6filler", "7filler", "8filler", "9filler", "c9cbcb"];
 
 
@@ -7,7 +9,30 @@ function updateBands() {
   //Update vars
   resitance = document.getElementById("resistance").value;
   tolerance = document.getElementById("tolerance").value;
+  getMultiplier();
+  //Band 3
+  document.getElementById("rect3").style.fill = band3colors[multiplierArrayNum];
   //Band 4
   document.getElementById("rect4").style.fill = band4colors[tolerance];
 }
 
+function getMulitplier() {
+  var multiplierLoopGoing = true;
+  var multiplierNum = 0.1;
+  var multiplierArrayNum = 0;
+  while (multiplierLoopGoing === true) {
+    multiplierNum = multiplierNum * 10;
+    multiplierArrayNum = multiplierArrayNum + 1;
+    if ((tolerance / multiplierNum) % 1 === 0) {
+      multiplier = multiplierNum;
+    }
+    else {
+      multiplierLoopGoing = false;
+      multiplierNum = multiplierNum / 10;
+      multiplierArrayNum = multiplierArrayNum - 1;
+    }
+    if (multiplierNum === 1000000) {
+      multiplierLoopGoing = false;
+    }
+  }
+}
