d888b283b2712bd4b0d0e56b098daecbfcac8262
Booligoosh <booligoosh@optusnet.com.au>
2016-06-12 11:19:52 +1000

    updated array with 0 value

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var resitance = document.getElementById("resistance").value;
 var tolerance = document.getElementById("tolerance").value;
-var band4colors = ["#6e2c00", "#ff0000", 3, 4, "#cc9933", 6, 7, 8, 9, "c9cbcb"];
+var band4colors = ["0filler", "#6e2c00", "#ff0000", "3filler", "4filler", "#cc9933", "6filler", "7filler", "8filler", "9filler", "c9cbcb"];
 
 
 function updateBands() {
