1ae3653e39c998c81ae3cad2327b63033642f45c
Booligoosh <booligoosh@optusnet.com.au>
2016-06-12 11:17:32 +1000

    added function updateBands()

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var tolerance = document.getElementById("tolerance").value;
 var band4colors = ["#6e2c00", "#ff0000", 3, 4, "#cc9933", 6, 7, 8, 9, "c9cbcb"];
 
 
+function updateBands() {
+  //Update vars
+  resitance = document.getElementById("resistance").value;
+  tolerance = document.getElementById("tolerance").value;
+  //Band 4
+  document.getElementById("rect4").style.color = band4colors[tolerance];
+}
 
-
-document.getElementById("rect4").style.color = band4colors[tolerance];
