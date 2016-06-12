8383fed5acce65b8ade2efded4a05a5dc2568419
Booligoosh <booligoosh@optusnet.com.au>
2016-06-12 12:55:25 +1000

    made updateBands() repeat forever

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ function updateBands() {
   document.getElementById("rect3").style.fill = band3colors[getMultiplier()];
   //Band 4
   document.getElementById("rect4").style.fill = band4colors[toleranceVar];
+  updateBands();
 }
 
 function getMultiplier() {
