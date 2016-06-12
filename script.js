5d2d801ee8de0d906452e70cdad30e18a747b501
Booligoosh <booligoosh@optusnet.com.au>
2016-06-12 12:40:57 +1000

    added band 1 and 2

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 var resistanceVar = document.getElementById("resistance").value;
 var multiplierVar = 0;
 var toleranceVar = document.getElementById("tolerance").value;
+var band1colors = ["#000000", "#6e2c00", "#ff0000", "#ff8000", "#ffff00", "#008000", "#0000ff", "#800080", "#646464", "#ffffff"];
+var band2colors = ["#000000", "#6e2c00", "#ff0000", "#ff8000", "#ffff00", "#008000", "#0000ff", "#800080", "#646464", "#ffffff"];
 var band3colors = ["0filler", "#000000", "#6e2c00", "#ff0000", "#ff8000", "#ffff00", "#008000", "#0000ff"];
 var band4colors = ["0filler", "#6e2c00", "#ff0000", "3filler", "4filler", "#cc9933", "6filler", "7filler", "8filler", "9filler", "c9cbcb"];
-var multiplierArrayNum
+var multiplierArrayNum;
+var band1Num;
+var band2Num;
 
 function updateBands() {
   //Update vars
+  getMultiplier()
+  if ((resistanceVar / multiplierVar).toString().length === 1) {
+    band1Num = 0;
+    band2Num = resistanceVar / multiplierVar;
+  }
+  else {
+    var number = resistanceVar / multiplierVar;
+    var output = [];
+    var sNumber = number.toString();
+    for (var i = 0, len = sNumber.length; i < len; i += 1) {
+        output.push(+sNumber.charAt(i));
+    }
+    band1Num = output[0];
+    band2Num = output[1];
+  }
   toleranceVar = document.getElementById("tolerance").value;
   resistanceVar = document.getElementById("resistance").value;
+  //Band 1
+  document.getElementById("rect3").style.fill = band3colors[band1Num];
+  //Band 2
+  document.getElementById("rect3").style.fill = band3colors[band2Num];
   //Band 3
   document.getElementById("rect3").style.fill = band3colors[getMultiplier()];
   //Band 4
