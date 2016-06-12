0b093b5ecd0a888a41757c8258213c74ff176ad8
Booligoosh <booligoosh@optusnet.com.au>
2016-06-12 11:12:28 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var resitance = document.getElementById("resistance").value;
+var tolerance = document.getElementById("tolerance").value;
+var band4colors = ["#6e2c00", "#ff0000", 3, 4, "#cc9933", 6, 7, 8, 9, "c9cbcb"];
+
+
+
+
+document.getElementById("rect4").style.color = band4colors[tolerance];
